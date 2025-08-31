9190d19457d72412d81ef8d6e817d2c6dc6dda57
hyeongseopE <gudtjq363636@naver.com>
2025-09-01 05:50:43 +0900

    feat[맴버 본인 구독 및 본인 파티 조인 알람 개발]

---
+++ src/lib/hooks/useMemberNotificationSocket.ts
@@ -0,0 +1,57 @@
+import useSWRSubscription from 'swr/subscription';
+import { WebSocketMessage } from './types';
+
+const WEBSOCKET_URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL;
+
+interface MemberNotificationSocketOptions {
+    onRoomJoined?: () => void;
+    isLoggedIn: boolean;
+}
+
+export const useMemberNotificationSocket = (options: MemberNotificationSocketOptions) => {
+    const { isLoggedIn, onRoomJoined  } = options;
+
+    const { error } = useSWRSubscription(
+        isLoggedIn ? `${WEBSOCKET_URL}/ws/member` : null,
+        (key, { next }) => {
+            if (!key) {
+                return () => {};
+            }
+
+            const socket = new WebSocket(key);
+
+            socket.onopen = () => {
+                console.log('Member WebSocket 연결 성공');
+            };
+
+            socket.onmessage = (event: MessageEvent) => {
+                try {
+                    const message: WebSocketMessage = JSON.parse(event.data);
+                    console.log('받은 메시지:', message);
+
+                    if (message.type === 'room_joined' && onRoomJoined) {
+                        onRoomJoined();
+                    }
+
+                } catch (e) {
+                    console.error('연결에 실패 하였습니다.:', e);
+                }
+            };
+
+            socket.onerror = (event: Event) => {
+                console.error('유저 웹 소켓 에러:', event);
+                next(new Error('Member WebSocket error'));
+            };
+
+            socket.onclose = (event) => {
+                console.log('유저 웹 소켓 연결끊기:', event);
+            };
+
+            return () => {
+                socket.close();
+            };
+        }
+    );
+
+    return { error };
+};
